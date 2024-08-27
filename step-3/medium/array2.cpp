411d1eaccabd081d7d604f5b601f4984bd463eb9
Parshant7 <parshantkhichi@gmail.com>
2024-08-27 18:40:08 +0530

    changes

--- step-3/medium/array2.cpp
+++ step-3/medium/array2.cpp
@@ -10,13 +10,29 @@ int main(){
     int end = len-1;
 
     while(start<end){
-        if(arr[start] == )
-        if(arr[start]==0){
-            start++;
+        int j = start+1;
+        while(j<end and arr[start]!=0){
+            if(arr[j]==0){
+                swap(arr[j], arr[start]);
+            }
+            j++;
         }
-        if(arr[end]==2){
-            end--;
+
+        j=end-1;
+        while(arr[end]!=2 && end>start){
+            if(arr[j]==2){
+                swap(arr[j],arr[end]);
+            }
+            j--;
         }
+
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+
+    for(auto it: arr){
+        cout<<it<<" ";
     }
 
     return 0;
